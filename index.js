b32fc10a7ad712c85ef2e53fc74eb199a1557be3
Megan <meganmoulos@gmail.com>
2022-06-16 11:13:11 -0700

    commit tree 2a6915881f5327ca418ac21fe29c6ff56d0c8ffb

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+
+const thankYouCards = [];
+
+function thankYou(names, eventName) {
+    for (let i = 0; i < names.length; i++) {
+        thankYouCards.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+        debugger;
+    }
+    return(thankYouCards);
+}
+
+thankYou(["Charlie", "Samip", "Ali"], "birthday");
+
+console.log(thankYouCards);
